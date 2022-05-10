541e8b4602cb84e35afe4dc39358afff6a165e9b
admin <admin@CHETUIWK1901>
2022-05-10 16:53:55 +0530

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        public int Recursion_NaturalNo(int val, int nth)
+        {
+            if(nth<1)
+            {
+                return val;
+            }
+            nth--;
+            Console.WriteLine(val);
+
+            return Recursion_NaturalNo(val+1,nth);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.WriteLine("Print first 10 natural no.s ");
+            obj.Recursion_NaturalNo(1, 10);
+        }
+    }
+}
+
